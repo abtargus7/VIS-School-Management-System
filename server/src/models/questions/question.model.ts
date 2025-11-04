c41f8d0c8a1e45a31eadc1ce1e3b15774bf18cba
abtargus7 <abtargus7@gmail.com>
2025-11-04 09:43:57 +0530

    add qustion model

---
+++ server/src/models/questions/question.model.ts
@@ -0,0 +1,51 @@
+import mongoose, { Schema, Model, Document} from 'mongoose'
+import { IQuestionType } from './questionType.model'
+import { QuestionType } from './questionType.model'
+
+interface IQuestion extends Document {
+    _id: String,
+    question: String,
+    answer?: String
+    grade: String,
+    subject: String,
+    chapter: String,
+    questionTpe: IQuestionType,
+    description: String,
+    createdAt: Date,
+    updatedAt: Date
+}
+
+const questionSchema: Schema<IQuestion> = new mongoose.Schema({
+    question: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    answer: {
+        type: String,
+    },
+    grade: {
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+    chapter: {
+        type: String,
+        required: true
+    },
+    questionTpe: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'QuestionType',
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true}) 
+
+export const Question: Model<IQuestion> = mongoose.model<IQuestion>('Question', questionSchema)
+export type { IQuestion }
