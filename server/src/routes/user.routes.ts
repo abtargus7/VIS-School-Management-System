6c09d6cd515c70962306eb0823435ef6ddc496c9
abtargus7 <abtargus7@gmail.com>
2026-01-02 08:37:53 +0530

    modified question model

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { registerUser, loginUser } from "../controlllers/user.controller";
+import { verifyAdmin } from "@/middlewares/auth.middleware";
 
 const router = Router();
 
 // Route to register a new user (Public - no auth required)
-router.post("/register", registerUser);
+router.post("/register", verifyAdmin, registerUser);
 
 // Route to login user (Public - no auth required)
 router.post("/login", loginUser);
