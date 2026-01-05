3d008b0e69d00f55fdccb5d1506b852f56244bff
abtargus7 <abtargus7@gmail.com>
2026-01-05 14:17:23 +0530

    uninstall radix-ui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,17 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel } from '@radix-ui/themes'
 
 
 createRoot(document.getElementById('root')!).render(
   <html>
     <body>
-      <Theme appearance='dark'>
+      
         <App />
-        <ThemePanel />
-      </Theme>
+        
+      
     </body>
   </html>
 
